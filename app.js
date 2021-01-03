698cd4fcedbae377e7d6f91b86de6dd507f5f55b
Huy <69767329+Huy-jpg@users.noreply.github.com>
2021-01-03 15:47:29 -0800

    commit on 'master'

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const body = document.body,
       switcher = document.querySelector('.switcher'),
       whiteTxt = document.querySelectorAll('.white-text'),
       topCards = document.querySelectorAll('.users'),
-      bottomCards = document.querySelectorAll('.page-overview');
+      bottomCards = document.querySelectorAll('.page-overview'),
+      names = document.querySelectorAll('.desaturate-color');
 
 switcher.addEventListener('click', change);
 
@@ -10,13 +11,16 @@ function change() {
   switcher.classList.toggle('active')
   body.classList.toggle('after-change')
 
+  names.forEach(name =>{
+    name.classList.toggle('names-color-changer')
+  });
+
   topCards.forEach(card => {
     card.classList.toggle('background-changer')
   });
 
-  bottomCards.forEach(cards => {
-    cards.classList.toggle('second-background-changer');
-    console.log(123)
+  bottomCards.forEach(card => {
+    card.classList.toggle('background-changer');
   })
 
   whiteTxt.forEach(text =>{
